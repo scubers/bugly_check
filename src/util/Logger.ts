e340cf4720fe0e7cdbc694025e2a2741c2b404b2
Jrwong <jr-wong@qq.com>
2021-03-23 17:37:51 +0800

    fix

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -2,7 +2,7 @@ import * as formatter from 'dateformat'
 
 export class Logger {
   static info(content: any): void {
-    var date = formatter(new Date(), 'yyyy-MM-dd HH:mm:ss')
+    var date = formatter(new Date(), 'yyyy-mm-dd HH:MM:ss')
     console.log(`[${date}]: ${content}`)
   }
 }
